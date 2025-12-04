6db2bf6f37862682de01c46a5105d27f17593994
nata <taiga37192@outlook.jp>
2025-12-05 01:07:12 +0900

    update:classname

--- DQXMaterialCalculator/src/test/java/com/example/demo/DqxMaterialCalculatorApplicationTests.java
+++ DQXMaterialCalculator/src/test/java/com/example/demo/DqxMaterialCalculatorApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DoxMaterialCalculatorApplicationTests {
+class DqxMaterialCalculatorApplicationTests {
 
 	@Test
 	void contextLoads() {
